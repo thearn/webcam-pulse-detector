86848d263bf1fdee72a00d3510bc57a9932a0eff
Tristan Hearn <tristanhearn@gmail.com>
2013-11-30 20:23:00 -0500

    finished basic signal processing

--- get_pulse.py
+++ get_pulse.py
@@ -1,80 +1,105 @@
 from lib.device import Camera
 from lib.processors_noopenmdao import findFaceGetPulse
-from lib.interface import plotXY, imshow, waitKey,destroyWindow, moveWindow
-import numpy as np      
+from lib.interface import plotXY, imshow, waitKey, destroyWindow, moveWindow
+import numpy as np
 import datetime
 
+
 class getPulseApp(object):
+
     """
     Python application that finds a face in a webcam stream, then isolates the
     forehead.
 
-    Then the average green-light intensity in the forehead region is gathered 
+    Then the average green-light intensity in the forehead region is gathered
     over time, and the detected person's pulse is estimated.
     """
+
     def __init__(self):
-        #Imaging device - must be a connected camera (not an ip camera or mjpeg
-        #stream)
-        self.camera = Camera(camera=0) #first camera by default
-        self.w,self.h = 0,0
+        # Imaging device - must be a connected camera (not an ip camera or mjpeg
+        # stream)
+        self.camera = Camera(camera=0)  # first camera by default
+        self.w, self.h = 0, 0
         self.pressed = 0
-        #Containerized analysis of recieved image frames (an openMDAO assembly)
-        #is defined next.
+        # Containerized analysis of recieved image frames (an openMDAO assembly)
+        # is defined next.
 
-        #This assembly is designed to handle all image & signal analysis,
-        #such as face detection, forehead isolation, time series collection,
-        #heart-beat detection, etc. 
+        # This assembly is designed to handle all image & signal analysis,
+        # such as face detection, forehead isolation, time series collection,
+        # heart-beat detection, etc.
 
-        #Basically, everything that isn't communication
-        #to the camera device or part of the GUI
-        self.processor = findFaceGetPulse(bpm_limits = [50,160],
-                                          data_spike_limit = 2500.,
-                                          face_detector_smoothness = 10.)  
+        # Basically, everything that isn't communication
+        # to the camera device or part of the GUI
+        self.processor = findFaceGetPulse(bpm_limits=[50, 160],
+                                          data_spike_limit=2500.,
+                                          face_detector_smoothness=10.)
 
-        #Init parameters for the cardiac data plot
+        # Init parameters for the cardiac data plot
         self.bpm_plot = False
         self.plot_title = "Cardiac info - raw signal, filtered signal, and PSD"
-        
-        
-        #Maps keystrokes to specified methods
+
+        # Maps keystrokes to specified methods
         #(A GUI window must have focus for these to work)
-        self.key_controls = {"s" : self.toggle_search,
-                             "d" : self.make_bpm_plot,
-                             "f" : self.write_csv,
+        self.key_controls = {"s": self.toggle_search,
+                             "d": self.toggle_display_plot,
+                             "f": self.write_csv,
                              "t": self.train}
-        
+
     def train(self):
         print "pca/ica train", self.processor.train()
-        
+
     def write_csv(self):
         """
         Writes current data to a csv file
         """
         fn = "Webcam-pulse" + str(datetime.datetime.now())
         fn = fn.replace(":", "_").replace(".", "_")
-        data = np.vstack((60*self.processor.freqs, self.processor.fft))
-        np.savetxt(fn  + ".csv", data, delimiter=',')
+        data = np.vstack((60 * self.processor.freqs, self.processor.fft))
+        np.savetxt(fn + ".csv", data, delimiter=',')
         print "Writing csv"
-        
 
     def toggle_search(self):
         """
         Toggles a motion lock on the processor's face detection component.
 
         Locking the forehead location in place significantly improves
-        data quality, once a forehead has been sucessfully isolated. 
+        data quality, once a forehead has been sucessfully isolated.
         """
         #state = self.processor.find_faces.toggle()
         state = self.processor.find_faces_toggle()
-        print "face detection lock =",not state
+        print "face detection lock =", not state
+
+    def toggle_display_plot(self):
+        """
+        Toggles the data display.
+        """
+        if self.bpm_plot:
+            print "bpm plot disabled"
+            self.bpm_plot = False
+            destroyWindow(self.plot_title)
+        else:
+            print "bpm plot enabled"
+            self.bpm_plot = True
+            self.make_bpm_plot()
+            moveWindow(self.plot_title, self.w, 0)
 
     def make_bpm_plot(self):
         """
         Creates and/or updates the data display
         """
-        self.processor.plot()
+        plotXY([[self.processor.times,
+                 self.processor.samples],
+                [self.processor.freqs,
+                 self.processor.fft]],
+               labels=[False, True],
+               showmax=[False, "bpm"],
+               label_ndigits=[0, 0],
+               showmax_digits=[0, 1],
+               skip=[3, 4],
+               name=self.plot_title,
+               bg=self.processor.slices[0])
 
-    def key_handler(self):    
+    def key_handler(self):
         """
         Handle keystrokes, as set at the bottom of __init__()
 
@@ -82,8 +107,8 @@ class getPulseApp(object):
         detected.
         """
 
-        self.pressed = waitKey(10) & 255 #wait for keypress for 10 ms
-        if self.pressed == 27: #exit program on 'esc'
+        self.pressed = waitKey(10) & 255  # wait for keypress for 10 ms
+        if self.pressed == 27:  # exit program on 'esc'
             print "exiting..."
             self.camera.cam.release()
             exit()
@@ -98,26 +123,26 @@ class getPulseApp(object):
         """
         # Get current image frame from the camera
         frame = self.camera.get_frame()
-        self.h,self.w,_c = frame.shape
+        self.h, self.w, _c = frame.shape
 
-        #display unaltered frame
-        #imshow("Original",frame)
+        # display unaltered frame
+        # imshow("Original",frame)
 
-        #set current image frame to the processor's input
+        # set current image frame to the processor's input
         self.processor.frame_in = frame
-        #process the image frame to perform all needed analysis
+        # process the image frame to perform all needed analysis
         self.processor.run()
-        #collect the output frame for display
+        # collect the output frame for display
         output_frame = self.processor.frame_out
 
-        #show the processed/annotated output frame
-        imshow("Processed",output_frame)
+        # show the processed/annotated output frame
+        imshow("Processed", output_frame)
 
-        #create and/or update the raw data display if needed
+        # create and/or update the raw data display if needed
         if self.bpm_plot:
             self.make_bpm_plot()
 
-        #handle any key presses
+        # handle any key presses
         self.key_handler()
 
 if __name__ == "__main__":
--- lib/processors_noopenmdao.py
+++ lib/processors_noopenmdao.py
@@ -1,13 +1,17 @@
 import numpy as np
-import time, cv2, mdp, pylab
+import time
+import cv2
+import mdp
+import pylab
+
 
 class findFaceGetPulse(object):
-    
-    def __init__(self, bpm_limits=[], data_spike_limit=250, 
+
+    def __init__(self, bpm_limits=[], data_spike_limit=250,
                  face_detector_smoothness=10):
-        
-        self.frame_in = np.zeros((10,10))
-        self.frame_out = np.zeros((10,10))
+
+        self.frame_in = np.zeros((10, 10))
+        self.frame_out = np.zeros((10, 10))
         self.fps = 0
         self.buffer_size = 250
         #self.window = np.hamming(self.buffer_size)
@@ -15,54 +19,52 @@ class findFaceGetPulse(object):
         self.times = []
         self.t0 = time.time()
         self.bpm = 0
-        self.face_cascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_alt.xml")
-        
-        self.face_rect = [1,1,2,2]
-        self.last_center = np.array([0,0])
-        self.last_wh = np.array([0,0])
+        self.face_cascade = cv2.CascadeClassifier(
+            "cascades/haarcascade_frontalface_alt.xml")
+
+        self.face_rect = [1, 1, 2, 2]
+        self.last_center = np.array([0, 0])
+        self.last_wh = np.array([0, 0])
         self.output_dim = 13
         self.trained = False
-        
+
         self.idx = 1
         self.find_faces = True
-        
+
     def find_faces_toggle(self):
         self.find_faces = not self.find_faces
         return self.find_faces
-        
+
     def get_faces(self):
         return
-    
-    def shift(self,detected):
-        x,y,w,h = detected
-        center =  np.array([x+0.5*w,y+0.5*h])
+
+    def shift(self, detected):
+        x, y, w, h = detected
+        center = np.array([x + 0.5 * w, y + 0.5 * h])
         shift = np.linalg.norm(center - self.last_center)
-        
+
         self.last_center = center
         return shift
-        
+
     def draw_rect(self, rect):
-        x,y,w,h = rect
-        cv2.rectangle(self.frame_out, (x, y), (x+w, y+h), (0,255,0), 3)
-        
-    def get_subface_coord(self,fh_x, fh_y, fh_w, fh_h):
-        x,y,w,h = self.face_rect
-        return [int(x + w * fh_x - (w*fh_w / 2.0)), 
-                    int(y + h * fh_y - (h*fh_h / 2.0)), 
-                    int(w * fh_w), 
-                    int(h * fh_h)]
-    
+        x, y, w, h = rect
+        cv2.rectangle(self.frame_out, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+    def get_subface_coord(self, fh_x, fh_y, fh_w, fh_h):
+        x, y, w, h = self.face_rect
+        return [int(x + w * fh_x - (w * fh_w / 2.0)),
+                int(y + h * fh_y - (h * fh_h / 2.0)),
+                int(w * fh_w),
+                int(h * fh_h)]
+
     def get_subface_means(self, coord):
         x, y, w, h = coord
-        subframe = self.frame_in[y:y+h,x:x+w,:]
-        v1 = np.mean(subframe[:,:,0])
-        v2 = np.mean(subframe[:,:,1])
-        #v3 = np.mean(subframe[:,:,2])
-        
-        v3 = np.std(subframe[:,:,0])
-        v4 = np.std(subframe[:,:,1])
-        #print v1,v2
-        return v1, v2, v3, v4
+        subframe = self.frame_in[y:y + h, x:x + w, :]
+        v1 = np.mean(subframe[:, :, 0])
+        v2 = np.mean(subframe[:, :, 1])
+        v3 = np.mean(subframe[:, :, 2])
+
+        return (v1 + v2 + v3) / 3.
 
     def train(self):
         self.trained = not self.trained
@@ -72,23 +74,23 @@ class findFaceGetPulse(object):
         data = np.array(self.data_buffer).T
         np.savetxt("data.dat", data)
         np.savetxt("times.dat", self.times)
-        freqs = 60.*self.freqs
-        idx = np.where((freqs > 50) & (freqs < 180))  
+        freqs = 60. * self.freqs
+        idx = np.where((freqs > 50) & (freqs < 180))
         pylab.figure()
         n = data.shape[0]
         for k in xrange(n):
-            pylab.subplot(n,1,k+1)
+            pylab.subplot(n, 1, k + 1)
             pylab.plot(self.times, data[k])
         pylab.savefig("data.png")
         pylab.figure()
         for k in xrange(self.output_dim):
-            pylab.subplot(self.output_dim,1,k+1)
+            pylab.subplot(self.output_dim, 1, k + 1)
             pylab.plot(self.times, self.pcadata[k])
         pylab.savefig("data_pca.png")
-        
+
         pylab.figure()
         for k in xrange(self.output_dim):
-            pylab.subplot(self.output_dim,1,k+1)
+            pylab.subplot(self.output_dim, 1, k + 1)
             pylab.plot(freqs[idx], self.fft[k][idx])
         pylab.savefig("data_fft.png")
         quit()
@@ -96,118 +98,84 @@ class findFaceGetPulse(object):
     def run(self):
         self.times.append(time.time() - self.t0)
         self.frame_out = self.frame_in
-        self.gray = cv2.equalizeHist(cv2.cvtColor(self.frame_in, 
+        self.gray = cv2.equalizeHist(cv2.cvtColor(self.frame_in,
                                                   cv2.COLOR_BGR2GRAY))
-        
+
         if self.find_faces:
             self.data_buffer, self.times, self.trained = [], [], False
-            detected = list(self.face_cascade.detectMultiScale(self.gray, 
-                                                  scaleFactor=1.3,
-                                                  minNeighbors=4,
-                                                  minSize=(50, 50), 
-                                                  flags=cv2.CASCADE_SCALE_IMAGE))
-        
+            detected = list(self.face_cascade.detectMultiScale(self.gray,
+                                                               scaleFactor=1.3,
+                                                               minNeighbors=4,
+                                                               minSize=(
+                                                                   50, 50),
+                                                               flags=cv2.CASCADE_SCALE_IMAGE))
+
             if len(detected) > 0:
-                detected.sort(key=lambda a:a[-1]*a[-2])
-                
+                detected.sort(key=lambda a: a[-1] * a[-2])
+
                 if self.shift(detected[-1]) > 10:
                     self.face_rect = detected[-1]
-            self.draw_rect(self.face_rect)
-            return 
-        if set(self.face_rect) == set([1,1,2,2]):
+            forehead1 = self.get_subface_coord(0.5, 0.18, 0.25, 0.15)
+            self.draw_rect(forehead1)
             return
-        
+        if set(self.face_rect) == set([1, 1, 2, 2]):
+            return
+
         x, y, w, h = self.face_rect
-        
-        forehead1 = self.get_subface_coord(0.4, 0.18, 0.25, 0.15)
-        forehead2 = self.get_subface_coord(0.6, 0.18, 0.25, 0.15)
-        cheek1 = self.get_subface_coord(0.3, 0.58, 0.15, 0.2)
-        cheek2 = self.get_subface_coord(0.7, 0.58, 0.15, 0.2)
-        
-        self.draw_rect(self.face_rect)
-        #self.draw_rect(forehead1)
-        #self.draw_rect(forehead2)
-        #self.draw_rect(cheek1)
-        #self.draw_rect(cheek2)
-        
-        vals = self.get_subface_means(forehead1) + \
-            self.get_subface_means(forehead2) + \
-            self.get_subface_means(cheek1) + \
-            self.get_subface_means(cheek2) + self.get_subface_means(self.face_rect) 
-        
+        self.slices = [self.gray[y:y + h, x:x + w]]
+
+        forehead1 = self.get_subface_coord(0.5, 0.18, 0.25, 0.15)
+        self.draw_rect(forehead1)
+
+        vals = self.get_subface_means(forehead1)
+
         self.data_buffer.append(vals)
         L = len(self.data_buffer)
         if L > self.buffer_size:
             self.data_buffer = self.data_buffer[-self.buffer_size:]
             self.times = self.times[-self.buffer_size:]
             L = self.buffer_size
-            
-            processed = np.array(self.data_buffer)
-            if not self.trained:
-                print "PCA Train...", self.fps
-                #self.pca = mdp.nodes.PCANode(output_dim=self.output_dim)
-                #self.pca = mdp.Flow([mdp.nodes.PCANode(output_dim=self.output_dim)])
-                #self.pca = mdp.Flow([mdp.nodes.JADENode(max_it=1500, white_comp=self.output_dim)])
-                #self.pca = mdp.Flow([mdp.nodes.NormalizeNode()])
-                self.pca = mdp.Flow([mdp.nodes.PCANode(output_dim=10)])
-                self.pca.train(processed)
-                self.trained = True
-            self.pcadata = self.pca.execute(processed).T
-        else:
-            self.pcadata = np.array(self.data_buffer).T
-            
+
+        processed = np.array(self.data_buffer)
+        self.samples = processed
         if L > 10:
- 
-            pcadata = np.zeros(self.pcadata.shape)
-            self.output_dim = pcadata.shape[0]
-            #print "ready"
-            
+            self.output_dim = processed.shape[0]
+            # print "ready"
+
             self.fps = float(L) / (self.times[-1] - self.times[0])
             even_times = np.linspace(self.times[0], self.times[-1], L)
-            for i in xrange(self.output_dim):
-                interpolated = np.interp(even_times, self.times, self.pcadata[i] )
-                interpolated = np.hamming(L) * interpolated
-                pcadata[i] = interpolated
-            self.fft = np.abs(np.fft.rfft(self.pcadata, axis=1))
-            self.freqs = float(self.fps)/L*np.arange(L/2 + 1)
-            
-            freqs = 60.*self.freqs
-            idx = np.where((freqs > 50) & (freqs < 180))  
-            
-            mx = [np.max(self.fft[k][idx]) for k in xrange(self.output_dim)]
-            #md = [np.median(self.fft[k][idx]) for k in xrange(self.output_dim)]
-            md = [np.percentile(self.fft[k][idx], 0.5) for k in xrange(self.output_dim)]
-            #md2 = [np.percentile(self.fft[k][idx], 0.75) for k in xrange(self.output_dim)]
-            st = [np.std(self.fft[k][idx]) for k in xrange(self.output_dim)]
-            vals = [[(mx[k]-md[k])/st[k],
-                     freqs[idx][np.where(self.fft[k][idx] == mx[k])][-1]] for k in xrange(self.output_dim)]            
-            vals.sort(reverse=True)
-            print vals
-            bpms = np.array([v[-1] for v in vals])
-            for i in xrange(len(vals)-1):
-                bpm = vals[i][-1]
-                if np.abs(self.bpm - bpm) > freqs[-1]/L:
-                    if bpm - vals[i+1][-1] > 0.4 and np.where(bpms==bpm)[0].size > 1:
-                        self.bpm = bpm
-                        break
-                    else:
-                        continue
-                else:
-                    self.bpm = bpm
-                    break
-                
-            print self.bpm
-            #pylab.show()
-            #quit()
-            #print np.abs(fft)
-            #print 
-            #print 60*self.freqs
-            
+            interpolated = np.interp(even_times, self.times, processed)
+            interpolated = np.hamming(L) * interpolated
+            raw = np.fft.rfft(interpolated)
+            phase = np.angle(raw)
+            self.fft = np.abs(raw)
+            self.freqs = float(self.fps) / L * np.arange(L / 2 + 1)
+
+            freqs = 60. * self.freqs
+            idx = np.where((freqs > 50) & (freqs < 180))
+
+            pruned = self.fft[idx]
+            phase = phase[idx]
+
+            pfreq = freqs[idx]
+            self.freqs = pfreq
+            self.fft = pruned
+            idx2 = np.argmax(pruned)
+
+            t = (np.sin(phase[idx2]) + 1.) / 2.
+            t = 0.9 * t + 0.1
+            alpha = t
+            beta = 1 - t
+
+            self.bpm = 1
             self.idx += 1
-            
-            
-            zerochan = 0*self.gray[y:y+h, x:x+w]
-            self.frame_out[y:y+h, x:x+w] = cv2.merge([zerochan,
-                                                      self.gray[y:y+h, x:x+w],
-                                                      zerochan])
-    
+
+            x, y, w, h = self.get_subface_coord(0.5, 0.18, 0.25, 0.15)
+            r = alpha * self.frame_in[y:y + h, x:x + w, 0]
+            g = alpha * \
+                self.frame_in[y:y + h, x:x + w, 1] + \
+                beta * self.gray[y:y + h, x:x + w]
+            b = alpha * self.frame_in[y:y + h, x:x + w, 2]
+            self.frame_out[y:y + h, x:x + w] = cv2.merge([r,
+                                                          g,
+                                                          b])
